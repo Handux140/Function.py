0c9e4a27e225f6086c402537b0fc1ced88b9a8db
Handux140 <160829278+Handux140@users.noreply.github.com>
2024-05-16 23:47:22 +0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,23 @@
+#simple_application function():
+
+def addition (x,y):
+    z= x + y
+    return z
+
+def substraction (x,y):
+    z= x - y
+    return z
+
+def mutiplication (x,y):
+    z= x *y
+    return z
+
+def division (x,y):
+ z= x/y
+ return z
+
+print(addition(2,4))
+print(substraction(2,4))
+print(mutiplication(2,4))
+print(division(2,4))
+
